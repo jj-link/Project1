275a47f3b9dd8b5479231697aee3c1f7e72b04dd
jj-link <joseph.j.link@gmail.com>
2022-05-10 16:28:08 -0400

    First commit. Initial setup

---
+++ app/src/main/java/com/revature/dao/IUserDao.java
@@ -0,0 +1,25 @@
+package com.revature.dao;
+
+import com.revature.models.User;
+
+import java.util.List;
+
+public interface IUserDao {
+
+    //create
+    void createUser(User u);
+
+    //read
+    List<User> getAllUsers();
+    User getUserByEmailOrUsername(String emailOrUsername);
+    User gerUserById(int user_id);
+
+
+    //update
+    User updateUser(User u);
+
+    //delete
+    void deleteUserByEmailOrUsername(String emailOrUsername);
+
+
+}
